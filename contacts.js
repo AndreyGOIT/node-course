9c0cc86d099d93a9deaed05584fbd2bda7592381
AndreyGOIT <ae.borga@gmail.com>
2023-02-19 13:13:29 +0200

    summary

--- contacts.js
+++ contacts.js
@@ -3,32 +3,45 @@ const path = require("path");
 
 const contactsPath = path.resolve("./db/contacts.json");
 
-function listContacts() {
-  fs.readFile(contactsPath)
-    .then((data) => console.log(data.toString()))
-    .catch((err) => console.log(err.message));
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf8");
+  console.table(data.toString());
 }
 
-function getContactById(contactId) {
-  fs.readFile(contactsPath, "utf8")
-    .then(
-      console.log((data) => data.toString().find(({ id }) => id === contactId))
-    )
-    .catch((err) => console.log(err.message));
+async function getContactById(contactId) {
+  const data = await fs.readFile(contactsPath, "utf8");
+  console.log(data);
+  try {
+    const contacts = JSON.parse(data);
+    console.log(contacts);
+
+    const contactById = contacts.find((contact) => contact.id === contactId);
+    console.log(contactById);
+  } catch {
+    (err) => console.log(err.message);
+  }
 }
 
-function removeContact(contactId) {
-  fs.writeFile(contactsPath, (data) =>
-    data.filter(({ id }) => id !== contactId)
-  )
-    .then((data) => console.log(data.toString()))
-    .catch((err) => console.log(err.message));
+async function removeContact(contactId) {
+  const data = await fs.readFile(contactsPath, "utf8");
+  // console.log(data);
+  try {
+    const contacts = JSON.parse(data);
+    const newContacts = contacts.filter(({ id }) => id !== contactId);
+    console.log(newContacts);
+  } catch {
+    (err) => console.log(err.message);
+  }
 }
 
-function addContact(name, email, phone) {
-  fs.appendFile(contactsPath, { name, email, phone })
-    .then((data) => console.log(data.toString()))
-    .catch((err) => console.log(err.message));
+async function addContact(name, email, phone) {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const contacts = JSON.parse(data);
+  contacts.push({ name, email, phone });
+  console.log(contacts);
+  // fs.appendFile(contactsPath, (name, email, phone), "utf8")
+  //   .then((data) => console.log(data.toString()))
+  //   .catch((err) => console.log(err.message));
 }
 
 module.exports = {
